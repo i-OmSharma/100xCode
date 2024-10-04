e35831118067a1fb722869d9a0c99d54643e5b4e
i-OmSharma <sharmaom1201@gmail.com>
2024-10-04 22:18:15 +0530

    DOM 2

---
+++ DOM/DOM2/localBack.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+app.get ("/sum", (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const sum = a + b;
+    res.send(sum.toString());
+
+});
+
+app.listen(3000, () => {
+    console.log("server running at 3000");
+    
+});
