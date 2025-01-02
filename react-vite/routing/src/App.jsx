5e7bcbe48d22839692811ef5513b98a2f0ca66e6
i-OmSharma <sharmaom1201@gmail.com>
2025-01-02 13:04:24 +0530

    basic client side routing

--- react-vite/routing/src/App.jsx
+++ react-vite/routing/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import { BrowsesrRouter, Routes, Route } from 'react-router-dom'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      hii Om...
-    </>
+    // Client side routing
+    <BrowsesrRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/landing" element={<Landing  />} />
+      </Routes>
+    </BrowsesrRouter>
   )
 }
 
