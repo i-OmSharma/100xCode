49efdb8d084089fcc4e3f433b7da04afac8b549d
i-OmSharma <sharmaom1201@gmail.com>
2025-01-02 13:18:37 +0530

    constant top-bar

--- react-vite/routing/src/App.jsx
+++ react-vite/routing/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowsesrRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Dashboard } from './components/Dashboard'
 import { Landing } from './components/Landing'
 
@@ -7,14 +7,30 @@ import { Landing } from './components/Landing'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    // Client side routing
-    <BrowsesrRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/landing" element={<Landing  />} />
-      </Routes>
-    </BrowsesrRouter>
+  // return (
+  //   // Client side routing
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path="/dashboard" element={<Dashboard />} /> // path is the url path that we visit, like / for home and /dashboard for dashboard and other stuffs, page we visit
+  //       <Route path="/landing" element={<Landing  />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // )
+
+  return ( 
+    // Fixed Top-Bar, it remains consistant.
+    <div>
+      <div style={{background: "blue", color: "white"}}>
+        hi this is top bar
+      </div>
+      // Client side routing
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} /> // path is the url path that we visit, like / for home and /dashboard for dashboard and other stuffs, page we visit
+          <Route path="/landing" element={<Landing  />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
