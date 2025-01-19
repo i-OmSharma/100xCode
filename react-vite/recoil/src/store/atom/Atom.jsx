59edce05f7934f398a5d49c58fc79a4da90ba194
i-OmSharma <sharmaom1201@gmail.com>
2025-01-19 23:57:00 +0530

    useRecoilValue

---
+++ react-vite/recoil/src/store/atom/Atom.jsx
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 104 
+})
+
+export const jobskAtom = atom({
+    key: "jobsAtom",
+    default: 0
+})
+
+export const notificationkAtom = atom({
+    key: "notificationkAtom",
+    default: 12
+})
+
+export const messagingAtom = atom({
+    key: "messagingAtom",
+    default: 0
+})
+
