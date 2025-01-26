568163a8358e8933c04c1382f195b5796ca3d5d4
i-OmSharma <sharmaom1201@gmail.com>
2025-01-26 23:39:14 +0530

    responsive css

--- react-vite/tailwind-prac/src/App.jsx
+++ react-vite/tailwind-prac/src/App.jsx
@@ -23,17 +23,29 @@ function App() {
     // </>
 
     //grid
-    <>
-    <div className='grid grid-cols-10'>
-      <div className='bg-red-500 col-span-4'>hi</div>
-      <div className='bg-green-500 col-span-4' >hey</div>
-      <div className='bg-cyan-500 col-span-2'>Hello</div>
-    </div>
+  //   <>
+  //   <div className='grid grid-cols-10'>
+  //     <div className='bg-red-500 col-span-4'>hi</div>
+  //     <div className='bg-green-500 col-span-4' >hey</div>
+  //     <div className='bg-cyan-500 col-span-2'>Hello</div>
+  //   </div>
 
-    <div className='flex'>
-      <div className='bg-red-500 w-[40%]'>hi</div>
-      <div className='bg-green-500 w-[40%]'>hey</div>
-      <div className='bg-cyan-500 w-[20%]'>Hello</div>
+  // <div>
+  // {/* //using flex
+  //   <div className='flex'>
+  //     <div className='bg-red-500 w-[40%]'>hi</div>
+  //     <div className='bg-green-500 w-[40%]'>hey</div>
+  //     <div className='bg-cyan-500 w-[20%]'>Hello</div>
+  //   </div> */}
+  // </div>
+
+  // </>
+
+  <>
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+    <div className='bg-red-500'>hi</div>
+    <div className='bg-green-500' >hey</div>
+    <div className='bg-cyan-500'>Hello</div>
     </div>
   </>
   )
