1e0d847cd32e651de0c62b6a7903ff1799005c9e
i-OmSharma <sharmaom1201@gmail.com>
2025-01-27 00:06:28 +0530

    multi-resposive

--- react-vite/tailwind-prac/src/App.jsx
+++ react-vite/tailwind-prac/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
   // </>
 
   <>
-    <div className='grid grid-cols-1 md:grid-cols-3'>
+    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
     <div className='bg-red-500 dark:bg-gren-500'>hi</div>
     <div className='bg-green-500' >hey</div>
     <div className='bg-cyan-500'>Hello</div>
