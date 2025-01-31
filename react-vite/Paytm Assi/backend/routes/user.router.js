73365b26522dfb305bd8f5417b33579d10738eee
i-OmSharma <sharmaom1201@gmail.com>
2025-02-01 00:17:19 +0530

    sign-in route

--- react-vite/Paytm Assi/backend/routes/user.router.js
+++ react-vite/Paytm Assi/backend/routes/user.router.js
@@ -31,7 +31,7 @@ userRouter.post("/signup", async(req, res) => {
         }
     
         const existingUser = await userAuth.findOne({
-            userName: body.userName
+            userName: body.userName.toLowerCase()
         })
     
         if(existingUser) {
@@ -44,21 +44,80 @@ userRouter.post("/signup", async(req, res) => {
     
         const newUser = await userAuth.create({
             ...body,
+            userName: body.userName.toLowerCase(),
             password: hashedPassword
         })
     
         const token = jwt.sign({
             userId: newUser._id
         }, JWT_SECRET, {expiresIn: "1h"})
-        res.json({
+        res.status(201).json({
             message: "User created succesfully",
             token: token
         })
     } catch(error) {
-        console.log("error in SignUp:", 500)
+        console.log("Error in SignUp:", error)
         res.status(500).json({message:"internal server error"})
         
     }
 })
 
 
+// SignIn
+
+
+const signInSchema = zod.object({
+    userName: zod.string(),
+    password: zod.string().min(6)
+})
+
+userRouter.post('/signin', async(req, res) => {
+    try{
+        const body = req.body
+
+        // validate Inputs
+        const {success} = signInSchema.safeParse(body)
+        if(!success) {
+            return res.status(400).json({
+                message: "Incorrect Credentials"
+            })
+        }
+        
+        //check user in DB
+        const existingUser = await userAuth.findOne({
+            userName: body.userName.toLowerCase()
+        })
+    
+        if(!existingUser) {
+            return res.status(401).json({
+                message: "User not exists"
+            })
+        }
+
+        //verifying password
+        const isPasswordValid = await bcrypt.compare(body.password, existingUser.password)
+        if(!isPasswordValid) {
+            return res.status(401).json({
+                message: "Invalid Username or Password"
+            })
+        }
+
+        //Genertate TOKEN
+        const token = jwt.sign({
+            userId: existingUser._id
+        }, JWT_SECRET, {expiresIn: "1h"})
+        res.status(200).json({
+            message: "User Signin succesfully",
+            token: token
+        })
+
+
+    } catch(error) {
+        console.log("Error sign-in", error)
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
+
+export default userRouter
