4b0ee345e0331c42adb4525453e18092beeb233d
i-OmSharma <sharmaom1201@gmail.com>
2025-02-08 19:27:52 +0530

    account routes

--- react-vite/Paytm Assi/backend/routes/index.js
+++ react-vite/Paytm Assi/backend/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./user.router.js"
+import accountRouter from "./account.router.js";
 
 const mainRouter = express.Router()
 
 router.use("/user", userRouter)
+router.use("/account", accountRouter)
 
 export default mainRouter
