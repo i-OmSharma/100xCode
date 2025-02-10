11de904ffd6a3aaf00e9aa18a7a5f43231153572
i-OmSharma <sharmaom1201@gmail.com>
2025-02-10 23:27:09 +0530

    no hooks

---
+++ react-vite/custom-hooks/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import React from 'react' // if  want to use React.Component 
+import { Component } from 'react' //if u want to use Component
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <MyComponent />
+    </>
+  )
+}
+
+class MyComponent extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {count:0}
+  }
+
+  incrementCount = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+
+      </div>
+    )
+  }
+}
+
+// function MyComponent() {
+//   const [count, setCount] = useState(0)
+
+//   const incrementCount = () => {
+//     setCount(count+1)
+//   }
+
+//   return(
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={incrementCount}>Increment</button>
+//     </div>
+//   )
+// }
+
+export default App
