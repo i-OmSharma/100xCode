269f6f9b432c2fae5f9db334ad4b11c35d92d350
i-OmSharma <sharmaom1201@gmail.com>
2025-03-16 19:36:41 +0530

    LifeCycle events

--- react-vite/custom-hooks/src/App.jsx
+++ react-vite/custom-hooks/src/App.jsx
@@ -1,42 +1,53 @@
-import { useState } from 'react'
-import React from 'react' // if  want to use React.Component 
+//import { useState } from 'react'
+import React, { useEffect, useState } from 'react' // if  want to use React.Component 
 import { Component } from 'react' //if u want to use Component
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const [render, setRender] = useState(true)
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setRender(false)
+    }, 5000);
+  }, []);
+
+  if (render) {
+    return <div>
+      2nd div
+    </div>
+  }
 
   return (
     <>
-      <MyComponent />
-    </>
+      {render ? <MyComponent /> : <div></div> } 
+    </> 
   )
 }
 
 
 //Without any hooks
-class MyComponent extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {count:0}
-  }
+// class MyComponent extends React.Component{
+//   constructor(props) {
+//     super(props)
+//     this.state = {count:0}
+//   }
 
-  incrementCount = () => {
-    this.setState({count: this.state.count + 1})
-  }
+//   incrementCount = () => {
+//     this.setState({count: this.state.count + 1})
+//   }
 
-  render() {
-    return (
-      <div>
-        <p>{this.state.count}</p>
-        <button onClick={this.incrementCount}>Increment</button>
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.state.count}</p>
+//         <button onClick={this.incrementCount}>Increment</button>
 
-      </div>
-    )
-  }
-}
+//       </div>
+//     )
+//   }
+// }
 
 
 
@@ -54,5 +65,24 @@ class MyComponent extends React.Component{
 //     </div>
 //   )
 // }
+
+// LifeCycle Events
+//functional Component
+
+function MyComponent() {
+  useEffect(() => {
+    console.error("Component Mounted")
+
+    return () => {
+      console.log("Component Unmounted")
+    } //u can return function inside the useEffect u can only return function nothing else, this function runs anytime the dependencies change or the component unmounts. It is used somewhat for cleaing up the previous mounted component.
+  },[])
+
+  return <div>
+    From inside my Component
+  </div>
+}
+
+
 
 export default App
