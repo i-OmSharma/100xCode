5496eaa5147eadfa88df49c76c405bcf8f28579b
i-OmSharma <sharmaom1201@gmail.com>
2025-04-01 14:36:28 +0530

    array

---
+++ TypeScript/node-app/src/array.ts
@@ -0,0 +1,13 @@
+// type numberArr = number[] another method
+
+function maxValue(arr: number[]) {
+    let max = 0;
+    for (let i = 0; i <arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max
+}
+
+console.log(maxValue([1, 2, 3]));
