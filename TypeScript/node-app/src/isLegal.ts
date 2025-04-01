37491b70c589b42227195c2b77e7be33a2e32fb1
i-OmSharma <sharmaom1201@gmail.com>
2025-04-01 13:01:32 +0530

    Interfaces

--- TypeScript/node-app/src/isLegal.ts
+++ TypeScript/node-app/src/isLegal.ts
@@ -1,9 +1,29 @@
-function isLegal(age: number): boolean {
-    if (age > 18) {
+// function isLegal(age: number): boolean {
+//     if (age > 18) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+interface User {
+    firstName: string,
+    lastName: string,
+    age: number
+}
+
+function isLegal(user: User): boolean {
+    if (user.age > 18) {
         return true
     } else {
         return false
     }
 }
 
-console.log(isLegal(21));
+console.log(isLegal({
+    firstName: "Om",
+    lastName: "Om",
+    age: 21
+}));
+
+
