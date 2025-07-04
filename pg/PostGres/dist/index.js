219ca93e6ed8e29fc112667cbcc8a6f71e67bf25
i-OmSharma <sharmaom1201@gmail.com>
2025-07-04 16:41:13 +0530

    prisma ORM

--- pg/PostGres/dist/index.js
+++ pg/PostGres/dist/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// function to create a user table in DB.
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,61 +8,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-const client = new pg_1.Client({
-    user: "om",
-    host: "localhost",
-    database: "mydb",
-    password: "pass",
-    port: 5432,
-});
-function createUserTable() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield client.connect();
-        const result = yield client.query(`
-            CREATE TABLE users (
-                id SERIAL PRIMARY KEY,
-                username VARCHAR(50) UNIQUE NOT NULL,
-                email VARCHAR(255) UNIQUE NOT NULL,
-                password VARCHAR(255) NOT NULL,
-                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-            )
-        `);
-        console.log(result);
-    });
-}
-function insertIntoTable() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield client.connect();
-        const insertQuery = `
-            INSERT INTO users (
-                username,
-                email,
-                password,
-                created_at
-            )
-            VALUES($1, $2, $3, CURRENT_TIMESTAMP)
-            RETURNING *;
-        `;
-        const Values = ['example', 'example@example.com', 'asdasd'];
-        const res = yield client.query(insertQuery, Values);
-        console.log('Insertion complete:', res.rows[0]);
-    });
-}
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            // await createUserTable()
-            yield insertIntoTable();
-        }
-        catch (err) {
-            console.log("ERROR", err);
-        }
-        finally {
-            yield client.end(); //always close connection with DB
-            console.log("Connection Closed");
-        }
-    });
-}
-main();
+const express_1 = __importDefault(require("express"));
+const db_1 = require("./db");
+const createUser_1 = require("./routes/createUser");
+const insertUser_1 = require("./routes/insertUser");
+const getUser_1 = require("./routes/getUser");
+const deleteUser_1 = require("./routes/deleteUser");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3000;
+app.use(express_1.default.json());
+app.post("/users", insertUser_1.insertUser);
+app.get("/users/:email", getUser_1.getUser);
+app.delete("/users/:username", deleteUser_1.deleteUser);
+app.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, db_1.connectToDB)();
+    yield (0, createUser_1.createUser)();
+    console.log(`Server is running at http://localhost:${port}`);
+}));
