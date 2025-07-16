4fa5390d41e32e0f160e594fe8b0660f158cbb84
i-OmSharma <sharmaom1201@gmail.com>
2025-07-16 22:32:52 +0530

    zod Inference

--- TypeScript/tsAPI/dist/index.js
+++ TypeScript/tsAPI/dist/index.js
@@ -1,37 +1,134 @@
 "use strict";
-function sumOfAge(User1, User2) {
-    return User1.age + User2.age;
-}
-function UpdateUser(id, updateData) {
-    //Db calls
-    console.log(`Updating user with ID: ${id}`);
-    console.log(`New name: ${updateData.name}`);
-    console.log(`New age: ${updateData.age}`);
-    console.log(`New password: ${updateData.password}`);
-}
-const updatePayload = {
-    name: "Amit",
-    age: 30,
-    password: "newPass123"
+//interface
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-UpdateUser(1, updatePayload);
-// or
-const obj = {
-    name: "john",
-    age: 21
-};
-const users = {
-    "ras@1": {
-        id: 1,
-        username: "rashid"
-    },
-    "ahm@2": {
-        id: 2,
-        username: "admad"
+Object.defineProperty(exports, "__esModule", { value: true });
+// interface user {
+//   id: number;
+//   name: string;
+//   age: number;
+//   email: string;
+//   password: string;
+// }
+// function sumOfAge(User1: user, User2: user) {
+//   return User1.age + User2.age;
+// }
+// const age = sumOfAge({age: 30}, {age: 21})
+// console.log(age);
+//Pick
+// type Updateprops = Pick<user, "name" | "age" | "password">;
+// function UpdateUser(id: number, updateData: Updateprops) {
+//   //Db calls
+//   console.log(`Updating user with ID: ${id}`);
+//   console.log(`New name: ${updateData.name}`);
+//   console.log(`New age: ${updateData.age}`);
+//   console.log(`New password: ${updateData.password}`);
+// }
+// const updatePayload: Updateprops = {
+//   name: "Amit",
+//   age: 30,
+//   password: "newPass123",
+// };
+// UpdateUser(1, updatePayload);
+//partial
+// interface user2 {
+//   id: number;
+//   name: string;
+//   age?: number;
+//   email?: string; //partial
+//   password: string;
+// }
+// //Readonly
+// type user3 = {
+//   // readonly id: number,
+//   name: string;
+//   age?: number;
+//   email?: string; //partial
+//   password?: string;
+// };
+// // or
+// const obj: Readonly<user3> = {
+//   name: "john",
+//   age: 21,
+// };
+// obj.age = 12
+//Object  --> Records and Maps
+// type User = {
+//     id: number,
+//     username: string
+// }
+// type Users = {
+//     [key: string]: User
+// }
+// const users: Users = {
+//     "ras@1": {
+//         id: 1,
+//         username: "rashid"
+//     },
+//     "ahm@2": {
+//         id: 2,
+//         username: "admad"
+//     }
+// }
+//Records
+// type User = {
+//   id: number;
+//   username: string;
+// };
+// type Users = Record<string, User>;
+// const users: Users = {
+//   "ras@1": {
+//     id: 1,
+//     username: "rashid",
+//   },
+//   "ahm@2": {
+//     id: 2,
+//     username: "admad",
+//   },
+// };
+//Map
+// type usr = {
+//   id: number;
+//   name: string;
+// };
+// const users1 = new Map<string, usr>();
+// users1.set("ras@1", { id: 1, name: "Rashid" });
+// users1.set("ahm@2", { id: 2, name: "ahmad" });
+// const usrs = users1.get("ras@1");
+// console.log(usrs);
+// //Exclude
+// type EventType = 'click' | 'scroll' | 'mousemove'
+// type ExcludeEvent = Exclude<EventType,'scroll' >
+// const handleEvent = (event: ExcludeEvent) => {
+//     console.log(`handling event: ${event}`); 
+// }
+// handleEvent('click') //Ok
+// handleEvent('scroll') //NotOK
+//Type inteface in zod
+const zod_1 = __importDefault(require("zod"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const userProfileSchema = zod_1.default.object({
+    name: zod_1.default.string().min(1, { message: "Name can't be empty" }),
+    email: zod_1.default.string().email({ message: "Invalid Email" }),
+    age: zod_1.default.number().min(18, { message: "under age" })
+});
+app.post("/user", (req, res) => {
+    const { success } = userProfileSchema.safeParse(req.body);
+    const updateBody = req.body;
+    if (!success) {
+        res.status(411).json({
+            message: "Inputs not correct"
+        });
+        return;
     }
-};
-const users1 = new Map();
-users1.set("ras@1", { id: 1, name: "Rashid" });
-users1.set("ahm@2", { id: 2, name: "ahmad" });
-const usrs = users1.get("ras@1");
-console.log(usrs);
+    console.log(updateBody);
+    res.json({
+        message: "User Updated"
+    });
+});
+app.listen(3000, () => {
+    console.log("App running at pport 3000");
+});
--- TypeScript/tsAPI/src/index.ts
+++ TypeScript/tsAPI/src/index.ts
@@ -1,62 +1,64 @@
 //interface
-interface user {
-  id: number;
-  name: string;
-  age: number;
-  email: string;
-  password: string;
-}
 
-function sumOfAge(User1: user, User2: user) {
-  return User1.age + User2.age;
-}
+// interface user {
+//   id: number;
+//   name: string;
+//   age: number;
+//   email: string;
+//   password: string;
+// }
+
+// function sumOfAge(User1: user, User2: user) {
+//   return User1.age + User2.age;
+// }
 
 // const age = sumOfAge({age: 30}, {age: 21})
 // console.log(age);
 
 //Pick
-type Updateprops = Pick<user, "name" | "age" | "password">;
 
-function UpdateUser(id: number, updateData: Updateprops) {
-  //Db calls
-  console.log(`Updating user with ID: ${id}`);
-  console.log(`New name: ${updateData.name}`);
-  console.log(`New age: ${updateData.age}`);
-  console.log(`New password: ${updateData.password}`);
-}
+// type Updateprops = Pick<user, "name" | "age" | "password">;
 
-const updatePayload: Updateprops = {
-  name: "Amit",
-  age: 30,
-  password: "newPass123",
-};
+// function UpdateUser(id: number, updateData: Updateprops) {
+//   //Db calls
+//   console.log(`Updating user with ID: ${id}`);
+//   console.log(`New name: ${updateData.name}`);
+//   console.log(`New age: ${updateData.age}`);
+//   console.log(`New password: ${updateData.password}`);
+// }
 
-UpdateUser(1, updatePayload);
+// const updatePayload: Updateprops = {
+//   name: "Amit",
+//   age: 30,
+//   password: "newPass123",
+// };
+
+// UpdateUser(1, updatePayload);
 
 //partial
-interface user2 {
-  id: number;
-  name: string;
-  age?: number;
-  email?: string; //partial
-  password: string;
-}
+// interface user2 {
+//   id: number;
+//   name: string;
+//   age?: number;
+//   email?: string; //partial
+//   password: string;
+// }
 
-//Readonly
-type user3 = {
-  // readonly id: number,
-  name: string;
-  age?: number;
-  email?: string; //partial
-  password?: string;
-};
+// //Readonly
+// type user3 = {
+//   // readonly id: number,
+//   name: string;
+//   age?: number;
+//   email?: string; //partial
+//   password?: string;
+// };
 
-// or
+// // or
 
-const obj: Readonly<user3> = {
-  name: "john",
-  age: 21,
-};
+// const obj: Readonly<user3> = {
+//   name: "john",
+//   age: 21,
+// };
 
 // obj.age = 12
 
@@ -84,45 +86,94 @@ const obj: Readonly<user3> = {
 
 //Records
 
-type User = {
-  id: number;
-  username: string;
-};
+// type User = {
+//   id: number;
+//   username: string;
+// };
 
-type Users = Record<string, User>;
+// type Users = Record<string, User>;
 
-const users: Users = {
-  "ras@1": {
-    id: 1,
-    username: "rashid",
-  },
-  "ahm@2": {
-    id: 2,
-    username: "admad",
-  },
-};
+// const users: Users = {
+//   "ras@1": {
+//     id: 1,
+//     username: "rashid",
+//   },
+//   "ahm@2": {
+//     id: 2,
+//     username: "admad",
+//   },
+// };
 
 //Map
-type usr = {
-  id: number;
-  name: string;
-};
 
-const users1 = new Map<string, usr>();
-users1.set("ras@1", { id: 1, name: "Rashid" });
-users1.set("ahm@2", { id: 2, name: "ahmad" });
+// type usr = {
+//   id: number;
+//   name: string;
+// };
 
-const usrs = users1.get("ras@1");
-console.log(usrs);
+// const users1 = new Map<string, usr>();
+// users1.set("ras@1", { id: 1, name: "Rashid" });
+// users1.set("ahm@2", { id: 2, name: "ahmad" });
 
-//Exclude
+// const usrs = users1.get("ras@1");
+// console.log(usrs);
 
-type EventType = 'click' | 'scroll' | 'mousemove'
-type ExcludeEvent = Exclude<EventType,'scroll' >
+// //Exclude
 
-const handleEvent = (event: ExcludeEvent) => {
-    console.log(`handling event: ${event}`); 
-}
+// type EventType = 'click' | 'scroll' | 'mousemove'
+// type ExcludeEvent = Exclude<EventType,'scroll' >
 
-handleEvent('click') //Ok
+// const handleEvent = (event: ExcludeEvent) => {
+//     console.log(`handling event: ${event}`); 
+// }
+
+// handleEvent('click') //Ok
 // handleEvent('scroll') //NotOK
+
+
+//Type inteface in zod
+
+import z from "zod"
+import e from "express"
+
+const app = e()
+app.use(e.json())
+
+const userProfileSchema = z.object({
+  name: z.string().min(1, {message: "Name can't be empty"}),
+  email:z.string().email({message: "Invalid Email"}),
+  age: z.number().min(18, {message: "under age"})
+})
+
+type FinalUserSchema = z.infer<typeof userProfileSchema>  // type infering the zod schema
+
+app.post("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body)
+
+  // const updateBody: {
+  //   name: string,
+  //   email: string,
+  //   age: number
+  // } = req.body
+
+  const updateBody: FinalUserSchema = req.body
+
+
+  if(!success) {
+    res.status(411).json({
+      message: "Inputs not correct"
+    })
+    return
+  }
+  
+  console.log(updateBody);
+
+  res.json({
+    message: "User Updated"
+  })
+})
+
+app.listen(3000, () => {
+  console.log("App running at pport 3000")
+})
+
